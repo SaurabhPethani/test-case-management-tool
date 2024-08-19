ae874133548e77a3bf5ec5148f706e4a6872defe
SaurabhPethani <spethani28@gmail.com>
2024-08-19 10:50:56 +0530

    refactored

--- test-case-management-backend/src/test/java/com/example/test_case_management/TestCaseManagementApplicationTests.java
+++ test-case-management-backend/src/test/java/com/example/test_case_management/TestCaseManagementApplicationTests.java
@@ -3,10 +3,8 @@ package com.example.test_case_management;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TestCaseManagementApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
